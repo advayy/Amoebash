2c0c6e16ca8c02698742ec06b4d15d9c8c044ea9
Saurav Banna <saurav05@students.cs.ubc.ca>
2025-02-22 21:31:42 -0800

    revert

--- src/common.hpp
+++ src/common.hpp
@@ -58,7 +58,7 @@ const float WINDOW_GRID_HEIGHT = WINDOW_HEIGHT_PX / TILE_SIZE;
 // MAP stuff
 const float CHUNK_DISTANCE = 5.0;
 const float MAP_WIDTH = 20;
-const float MAP_HEIGHT = 5;
+const float MAP_HEIGHT = 20;
 const vec2 WORLD_ORIGIN = {0, 0};
 
 // Set map left right based on width and origin
